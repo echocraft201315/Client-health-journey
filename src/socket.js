3f6b703d8a71dec122301dbc059effcababb98da
echocraft201315 <sachkhiha33@gmail.com>
2025-07-14 08:24:52 -0700

    aa

--- src/socket.js
+++ src/socket.js
@@ -2,4 +2,9 @@
 
 import { io } from "socket.io-client";
 
-export const socket = io(process.env.NEXTAUTH_URL);
+const socketUrl =
+    process.env.SOCKET_URL ||
+    process.env.NEXTAUTH_URL ||
+    (typeof window !== "undefined" ? window.location.origin : "");
+
+export const socket = io(socketUrl);
